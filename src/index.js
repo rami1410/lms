92536cacee91851a355d06e561aef8b29cd3f091
rami1410 <rami@robotix.co.il>
2026-05-22 08:06:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="he" dir="rtl">
-  <head>
-    <meta charset="utf-8" />
-    
-    <link rel="icon" href="https://i.postimg.cc/k5GvHx7n/Screenshot-2026-05-21-22-51-14.png" />
-    <link rel="apple-touch-icon" href="https://i.postimg.cc/k5GvHx7n/Screenshot-2026-05-21-22-51-14.png" />
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#0f172a" />
-    <meta
-      name="description"
-      content="מערכת ניהול למידה - חותם חיים מבית רובוטיקס"
-    />
-    
-    <title>חותם חיים מבית רובוטיקס</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
